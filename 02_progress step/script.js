4ead3771effbd4e0e544992ef3c81f5922b2c775
HHhahei <1185471581@qq.com>
2022-09-04 19:02:07 +0800

    02_progress step

---
+++ 02_progress step/script.js
@@ -0,0 +1,42 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circle = document.querySelectorAll(".circle");
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
+  }
+  update()
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update()
+});
+
+function update() {
+  circle.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const active = document.querySelectorAll(".active");
+  progress.style.width =
+    ((active.length - 1) / (circle.length - 1)) * 100 + "%";
+  if(currentActive === 1){
+    prev.disabled = true
+  }else if(currentActive === circle.length){
+    next.disabled = true
+  }else{
+    prev.disabled = false
+    next.disabled = false
+  }
+}
