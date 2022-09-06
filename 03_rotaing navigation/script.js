35cd98253355354088660f97983d23c461f02f9f
HHhahei <1185471581@qq.com>
2022-09-07 00:48:49 +0800

    03_rotaing navigation

---
+++ 03_rotaing navigation/script.js
@@ -0,0 +1,7 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+console.log(container.classList)
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'))
